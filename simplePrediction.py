56d753c177c16e2d0a012dacdb2f4f534efbde30
benja <benja1006@gmail.com>
2023-04-11 11:06:06 -0700

    Updated simplePrediciton
    Now returns the mean of each column. Made a group folder in HPC, need to copy data

--- simplePrediction.py
+++ simplePrediction.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import os
 
-filepath = os.path.join(os.getcwd(), "./simplePrediction.py")
+filepath = os.path.join(os.getcwd(), "SampleLables.csv")
 
 df = pd.read_csv(filepath, sep=',', header='infer')
 
-# df_labels = df[-9,]
+df_labels = df[df.columns[-9:]]
 
-print(df)
+means = df_labels.mean(axis=0, skipna=True)
+
+print(means)
