e64d5a5d26597a63c326ace4c6b3467b37b1729c
Ekugelsk <ekugelsk@caltech.edu>
2023-04-12 11:05:50 -0700

    dadaloader

--- XRAYdataLoader.py
+++ XRAYdataLoader.py
@@ -1,11 +1,14 @@
 import os
 import pandas as pd
 from torchvision.io import read_image
+from torch.utils.data import Dataset, DataLoader
 
-class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+
+class XrayDataset(Dataset):
+    
+    def __init__(self, annotations_file, transform=None, target_transform=None):
         self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
+        # self.img_dir = self.img_labels["Path"]
         self.transform = transform
         self.target_transform = target_transform
 
@@ -13,11 +16,18 @@ class CustomImageDataset(Dataset):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
+        labelX = self.img_labels.iloc[idx].to_dict()
+        # img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(label["Path"])
+        pid = label["Path"].split("/")[1]
+        
+        X = (pid,label["Sex"], label["Age"], label["Frontal/Lateral"],label["AP/PA"])
+        
+        Y = (label["No Finding"], label['Enlarged Cardiomediastinum'], label["Cardiomegaly"], label['Lung Opacity'],
+            label['Pneumonia'], label['Pleural Effusion'], label['Pleural Other'], label['Fracture'], label['Support Devices'])
+        
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
-        return image, label
+        return image, X, Y
