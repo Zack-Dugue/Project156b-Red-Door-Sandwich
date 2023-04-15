5f65c8364131fef792f6103acc43a29bad7aea53
Benja Share <benja1006@gmail.com>
2023-04-15 16:19:11 -0700

    Updated SimplePrediction to run locally
    Changed the file paths back, and updated it so it outputs correctly

--- simplePrediction.py
+++ simplePrediction.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-filepath = os.path.join(os.getcwd(), "/central/groups/CS156b/2023/Red_Door_Sandwich/data/student_labels/train2023.csv")
+filepath = os.path.join(os.getcwd(), "data/train2023.csv")
 
 df = pd.read_csv(filepath, sep=',', header='infer')
 
@@ -12,7 +12,7 @@ means = data_cols.mean(axis=0, skipna=True)
 
 # print(means)
 
-test_ids_filepath = '/central/groups/CS156b/2023/Red_Door_Sandwich/data/student_labels/test_ids.csv'
+test_ids_filepath = 'data/test_ids.csv'
 output_df = pd.read_csv(test_ids_filepath, sep=',', header='infer')
 
 output_data  = data_cols.iloc[:0,:].copy()
@@ -23,6 +23,7 @@ for idx, series in output_df.iterrows():
     output_data.loc[idx] = means 
 
 # print(output_data)
+output_df = output_df.drop('Path', axis=1)
 combined  = pd.concat([output_df, output_data], axis=1)
 
-combined.to_csv("/groups/CS156b/2023/Red_Door_Sandwich/Project156b-Red-Door-Sandwich/outputs/SampleOutput.csv", sep=',', header=True, index=False)
+combined.to_csv("outputs/SampleOutput.csv", sep=',', header=False, index=False)
