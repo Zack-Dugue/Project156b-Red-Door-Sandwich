d0d2775b244f10f20924254e7279eb200d5159f2
saras07 <92961600+saras07@users.noreply.github.com>
2023-04-27 16:23:45 -0700

    Update XRAYdataLoader.py

--- XRAYdataLoader.py
+++ XRAYdataLoader.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader , Dataset
 import pandas as pd
+import torchvision as tv
 from torchvision.io import read_image
 from torch.utils.data import Dataset, DataLoader
 
@@ -47,14 +48,20 @@ def make_custom_dataloader(*args, train = True):
     """
     dataset = CustomImageDataset()
     # return DataLoader(dataset, batch_size, shuffle=True )
-def train_image_transform(*args):
+    
+def train_image_transform(crop_size, rot_deg_range, hflip_p):
     """
     Returns a transfrom which performs image augmentations during training
-    :param args: whatever args you think are appropriate
+    :param args: 
+        - crop_size: tuple of crop size
+        - rot_deg_range: degree range to rotate the image
+        - hflip_p: probability of doing a horizontal flip
     :return:
     """
     transform = tv.transforms.Compose([
-        #TODO:
+        tv.transforms.RandomResizedCrop(size=crop_size), 
+        tv.transforms.RandomRotation(degrees=rot_deg_range), 
+        tv.transforms.RandomHorizontalFlip(p=hflip_p)
     ])
     return transform
 
