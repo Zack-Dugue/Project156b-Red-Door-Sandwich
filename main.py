3339dd595117fbddfc8da667fbbed0d33e71788c
benja <benja1006@gmail.com>
2023-04-28 15:43:37 -0700

    tried converting the losses tensor to numpy

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ class XrayModule(LightningModule):
         # pathologies
         losses = th.mean(losses,dim=0)
         loss = losses.mean()
+        losses = losses.detach().numpy() # remove gradients and return to numpy array
 
         tensorboard_logs = {'train_loss':loss, 'Enlarged Cardiomediastinum Loss' : losses[0],"Cardiomegaly" : losses[1],
                             "Lung Opacity" :losses[2],"Pneumonia" : losses[3],"Pleural Effusion" : losses[4],
@@ -68,12 +69,12 @@ def experiment(path,model_name, num_nodes,num_dataloaders,batch_size,learning_ra
         strategy = "auto"
     else:
         strategy = pl.DDPStrategy(static_graph = False)
-    trainer = pl.Trainer(accelerator = accelerator, devices=devices, max_epochs = num_epochs, strategy=strategy, num_nodes=num_nodes)
+    trainer = pl.Trainer(accelerator = accelerator, devices=devices, max_epochs = num_epochs, strategy=strategy, num_nodes=num_nodes, log_every_n_steps=1)
     # ANNOTATIONS_LABELS = "C:\\Users\\dugue\\PycharmProjects\\Project156b-Red-Door-Sandwich\\data\\student_labels\\train_sample.csv"
-    ANNOTATIONS_LABELS = os.path.join(os.getcwd(), 'data', 'student_labels', 'train.csv')
+    ANNOTATIONS_LABELS = os.path.join(os.getcwd(), 'data', 'student_labels', 'train_sample.csv')
     train_loader = make_dataloader(ANNOTATIONS_LABELS, batch_size,train=True)
     # ANNOTATIONS_LABELS = "C:\\Users\\dugue\\PycharmProjects\\Project156b-Red-Door-Sandwich\\data\\student_labels\\train_sample.csv"
-    ANNOTATIONS_LABELS = os.path.join(os.getcwd(), 'data', 'student_labels', 'train.csv')
+    ANNOTATIONS_LABELS = os.path.join(os.getcwd(), 'data', 'student_labels', 'train_sample.csv')
     #For now training and validation are done on the same dataset
     validation_loader = make_dataloader(ANNOTATIONS_LABELS, batch_size,train=False)
     xray_model = XRAYModel(NUM_CLASSES)
@@ -97,7 +98,7 @@ if __name__ == "__main__":
         num_dataloaders = 1
         batch_size = 32
         lr = .001
-        NumEpochs = 100
+        NumEpochs = 20
     else:
         args = sys.argv[1]
         path, model_name, num_nodes, num_dataloaders, batch_size, lr, NumEpochs = args[1:]
